19b1ad33d05d34ee37a38a11d7c796c3d0b0e28e
Yuliia Koriavets <112959119+YuliiaKoriavets@users.noreply.github.com>
2022-12-26 19:09:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,10 +6,10 @@ const feedbackFormEl = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 
-const formData = {};
+let formData = {};
 
-const handleInputForm = (event) => {
- formData[event.target.name] = event.target.value;
+const handleInputForm = event => {
+  formData[event.target.name] = event.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
@@ -18,25 +18,31 @@ const handleSubmitForm = event => {
   console.log(formData);
   localStorage.removeItem('feedback-form-state');
   feedbackFormEl.reset();
+  formData = {};
 };
 
 const load = key => {
   try {
     const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
+    return JSON.parse(serializedState);
   } catch (error) {
     console.error('Get state error: ', error.message);
   }
 };
 
-const storageData = load('feedback-form-state')
+const storageData = load('feedback-form-state');
 
-  if (storageData) {
-    email.value =  storageData.email;
-    message.value = storageData.message;
-  }
+if (storageData) {
+     email.value = storageData.email;
+  message.value = storageData.message;
+  
+  if (!storageData.email) {
+    email.value = '';
+  } else if (!storageData.message) {
+    message.value = '';
+  } 
+
+}
 
 feedbackFormEl.addEventListener('input', throttle(handleInputForm, 500));
 feedbackFormEl.addEventListener('submit', handleSubmitForm);
-
-
